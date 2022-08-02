444a08bb88ee42b533cd19dc96c50bf952d8ec53
Josh Pierce <joshua.alan.pierce@gmail.com>
2022-08-02 15:53:19 -0600

    Add printing

--- variable.js
+++ variable.js
@@ -1,6 +1,14 @@
 var age = 27
 var carsOwned = 1
 var name = "Josh"
+var lastName = "Price"
+var depositPaid = true
+var studentEnrolled = true
+var name = "Joshua"
 var lastName = "Pierce"
-var depositPaid = True
-var studentEnrolled = True
+console.log(age)
+console.log(carsOwned)
+console.log(name)
+console.log(lastName)
+console.log(depositPaid)
+console.log(studentEnrolled)
